569286bd96057ec0f15e9fb8178b3b579f295974
Joan Yassi <joan@tray.io>
2022-05-24 16:04:19 +0100

    Created first route

--- API/index.js
+++ API/index.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
+import postRoutes from './routes/posts.js'
+
 const app = express()
 dotenv.config()
 
@@ -18,3 +20,5 @@ const PORT = process.env.PORT || 5000
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch(error => console.log(error.message) )
+
+app.use('/post', postRoutes)
