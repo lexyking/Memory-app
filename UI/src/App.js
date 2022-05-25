3ff132cc2b08b5ce90d30150bc63a05781a89d9d
Joan Yassi <joan@tray.io>
2022-05-25 11:21:35 +0100

    Added material UI

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,8 +1,13 @@
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+
 const App = () => {
   return (
-    <div>
-      Hello from App
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>Memories</Typography>
+        <img height='60' src="" alt="memories"/>
+      </AppBar>
+    </Container>
   )
 }
 
