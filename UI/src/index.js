80f9941d8507b5181670c26b30543ce498f1b6e3
Joan Yassi <joan@tray.io>
2022-05-26 22:40:42 +0100

    configure redux

--- UI/src/index.js
+++ UI/src/index.js
@@ -1,5 +1,9 @@
-import App from './App'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import App from './App'
 
+import reducers from './reducers'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
