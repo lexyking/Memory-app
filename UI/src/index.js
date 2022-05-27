75745e4c0b7249f3b1cd867d7893feea350e3706
Joan Yassi <joan@tray.io>
2022-05-27 23:17:09 +0100

    Set up the reducer

--- UI/src/index.js
+++ UI/src/index.js
@@ -5,5 +5,11 @@ import thunk from 'redux-thunk'
 import App from './App'
 
 import reducers from './reducers'
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  )
