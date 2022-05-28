2e62a3a7de0cd88d3537a3e3fe25b1f6ccef30c1
Joan Yassi <joan@tray.io>
2022-05-28 22:12:52 +0100

    Created the memory form

--- UI/src/components/form/form.js
+++ UI/src/components/form/form.js
@@ -1,12 +1,29 @@
+import { Paper, TextField, Button, Typography } from '@mui/material'
 // import useStyles from './styles'
 
 const Form = () => {
   // const classes = useStyles()
+  const handleSubmit = () => {
+    console.log('submited')
+  }
 
   return (
-    <div>
-      Hello from single Form
-    </div>
+    <Paper>
+      <form autoComplete='off' noValidate onSubmit={handleSubmit}>
+        <Typography variant='h6'>
+          Creating Memory
+        </Typography>
+        <TextField 
+          name='creator'
+          variant='outlined'
+          label='Creator'
+          fullWidth
+          value={}
+          onChange={handleChange}
+          ></TextField>
+      </form>
+
+    </Paper>
   )
 }
 
