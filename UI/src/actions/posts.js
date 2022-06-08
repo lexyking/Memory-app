d358c8c706b446054a2165852f4400d49ffe62f3
Joan Yassi <joan@tray.io>
2022-06-08 17:17:24 +0100

    Added the constants for the actions types

--- UI/src/actions/posts.js
+++ UI/src/actions/posts.js
@@ -1,11 +1,18 @@
 import * as api from '../api'
+import {
+  FETCH_ALL,
+  CREATE_POST,
+  LIKE_POST,
+  UPDATE_POST,
+  DELETE_POST
+} from '../constants/actionTypes'
 
 // Action creators
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts()
 
-    const action = { type: 'FETCH_ALL', payload: data }
+    const action = { type: FETCH_ALL, payload: data }
     dispatch(action)
   } catch (error) {
     console.log(error)
@@ -15,7 +22,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post)
-    dispatch({ type: 'CREATE_POST', payload: data})
+    dispatch({ type: CREATE_POST, payload: data})
   } catch (error) {
     console.log(error)
   }
@@ -24,7 +31,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post)
-    dispatch({ type: 'UPDATE_POST', payload: data})
+    dispatch({ type: UPDATE_POST, payload: data})
   } catch (error) {
     console.log(error)
   }
@@ -33,7 +40,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id)
-    dispatch({ type: 'DELETE_POST', payload: id})
+    dispatch({ type: DELETE_POST, payload: id})
   } catch (error) {
     console.log(error)
   }
@@ -42,7 +49,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const getLikePost = (id) => async (dispatch) => {
   try {
     const {data} = await api.getLikePost(id)
-    dispatch({ type: 'LIKE_POST', payload: data})
+    dispatch({ type: LIKE_POST, payload: data})
   } catch (error) {
     console.log(error)
   }
--- UI/src/reducers/posts.js
+++ UI/src/reducers/posts.js
@@ -1,13 +1,21 @@
+import {
+  FETCH_ALL,
+  CREATE_POST,
+  LIKE_POST,
+  UPDATE_POST,
+  DELETE_POST
+} from '../constants/actionTypes'
+
 const reducer = (posts = [], action) => {
   switch (action.type) {
-    case 'FETCH_ALL':
+    case FETCH_ALL:
       return action.payload
-    case 'CREATE_POST':
+    case CREATE_POST:
       return [...posts, action.payload]
-    case 'LIKE_POST':
-    case 'UPDATE_POST':
+    case LIKE_POST:
+    case UPDATE_POST:
       return posts.map(post => post._id === action.payload._id ? action.payload : post)
-    case 'DELETE_POST':
+    case DELETE_POST:
       return posts.filter(post => post._id !== action.payload._id)
     default:
       return posts
